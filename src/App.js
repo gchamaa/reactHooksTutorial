60e4f8caa7957a78b8a2b8fa687c087179d31fd6
Gilbert Chamaa <gchamaa@yahoo.com>
2021-10-08 13:19:25 -0700

    docs: Adding examples for useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // Example 1
+  const [name, setName] = useState("nothing");
+  const handleChangeExample1 = (e) => {
+    setName(e.target.value || "nothing");
+  }
+
+  // Example 2
+  const [values, setValues] = useState({firstName: "", lastName: ""});
+  const handleChangeExample2 = (e) => {
+    setValues({firstName: e.target.value});
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{paddingTop: "100px"}}>
+      <h2>Example - Basic useState with primitive value</h2>
+      <input type="text" onChange={handleChangeExample1} />
+      <p>You typed {name}</p>
+
+      <h2>Example - Complex useState with Object</h2>
+      <input type="text" onChange={handleChangeExample2} />
+      <p>First Name is {values.firstName}</p>
+      <p>Last Name is {values.lastName}</p>
     </div>
   );
 }
